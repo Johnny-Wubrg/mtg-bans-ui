fccb422c270e58a13869f29e048489312f00cae2
quangdaon <me@quangdao.com>
2024-12-28 21:11:35 -0600

    Homepage nav

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,10 +12,28 @@
 		You have stumbled upon an ongoing project to document and catalog the entire timeline of
 		banned and restricted <b>Magic: the Gathering™</b> cards. Feel free to poke around.
 	</p>
+
+	<div class="current">
+		<h3>Current Utilities:</h3>
+		<ul>
+			<li><a href="/announcements">Announcements Timeline</a></li>
+			<li><a href="/banlist">Temporal Banlist</a></li>
+		</ul>
+	</div>
 </section>
 
 <style>
   p {
     text-align: center;
+  }
+
+  .current {
+    max-width: fit-content;
+    margin: auto;
+
+    ul {
+      margin: 0;
+      padding: 0;
+    }
   }
 </style>
