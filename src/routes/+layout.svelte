7c81972622089447e0e31694e44bd3893243489d
quangdaon <me@quangdao.com>
2024-12-28 20:55:22 -0600

    Plausible

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,16 @@
 	import '../style/main.scss';
 	import AppHeader from '../components/layout/AppHeader.svelte';
 	import AppFooter from '../components/layout/AppFooter.svelte';
+	import { dev } from '$app/environment';
 
 	let { children } = $props();
 </script>
+
+<svelte:head>
+	{#if !dev}
+		<script defer data-domain="mtgbans.info" src="https://plausible.io/js/script.js"></script>
+	{/if}
+</svelte:head>
 
 <div class="app">
 	<AppHeader />
