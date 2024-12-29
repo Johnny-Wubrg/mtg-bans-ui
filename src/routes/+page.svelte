872b57c448b3a46e2cc24e91e6eb42abfaabed01
quangdaon <me@quangdao.com>
2024-12-28 20:50:40 -0600

    Center text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,10 +9,13 @@
 <section>
 	<PageTitle>Hello world!</PageTitle>
 	<p>
-		You have stumbled upon an ongoing project to document, catalog, and eventually visualize the entire timeline of
+		You have stumbled upon an ongoing project to document and catalog the entire timeline of
 		banned and restricted <b>Magic: the Gathering™</b> cards. Feel free to poke around.
 	</p>
 </section>
 
 <style>
+  p {
+    text-align: center;
+  }
 </style>
