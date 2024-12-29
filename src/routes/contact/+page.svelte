2f8ab849aba7f85846e4bf347b9a5bcd3291b538
quangdaon <me@quangdao.com>
2024-12-28 22:38:40 -0600

    AJAX form submit

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import PageTitle from '../../components/layout/PageTitle.svelte';
+	import { goto } from '$app/navigation';
 
 	interface ReasonDefinition {
 		label: string;
@@ -10,6 +11,28 @@
 		correction: { label: 'Submit a Correction or Citation' },
 		bug: { label: 'Report a Bug' }
 	};
+
+	const handleSubmit = async (event: SubmitEvent) => {
+		event.preventDefault();
+
+		const myForm = event.target as HTMLFormElement;
+		const formData = new FormData(myForm);
+
+		try {
+			const res = await fetch('/', {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+				body: new URLSearchParams(formData as any).toString()
+			});
+
+			if (!res.ok) throw new Error(res.statusText);
+
+			await goto('/contact/thank-you');
+		} catch (e) {
+			alert('There was a problem submitting the form. Please try again later.');
+		}
+	};
+
 </script>
 
 <svelte:head>
@@ -18,7 +41,14 @@
 
 <PageTitle>Contact</PageTitle>
 
-<form name="mtg-bans-contact" action="/contact/success" method="POST" data-netlify="true" netlify-honeypot="bot-field">
+<form
+	name="mtg-bans-contact"
+	action="/contact/thank-you"
+	method="POST"
+	data-netlify="true"
+	netlify-honeypot="bot-field"
+	onsubmit={(evt) => handleSubmit(evt)}>
+	<input type="hidden" name="form-name" value="mtg-bans-contact" />
 	<div class="field">
 		<label for="contact-subject">Subject</label>
 		<select name="subject" id="contact-subject" required>
@@ -47,25 +77,25 @@
 </form>
 
 <style>
-    .field {
-        margin: 2em 0;
+  .field {
+    margin: 2em 0;
 
-        label {
-            margin-bottom: 0.25em;
-        }
-
-        label, select, input, textarea {
-            display: block;
-            width: 100%;
-        }
-
-        option {
-            color: var(--color-black);
-        }
-
-        textarea {
-            resize: vertical;
-            min-height: 8em;
-        }
+    label {
+      margin-bottom: 0.25em;
     }
+
+    label, select, input, textarea {
+      display: block;
+      width: 100%;
+    }
+
+    option {
+      color: var(--color-black);
+    }
+
+    textarea {
+      resize: vertical;
+      min-height: 8em;
+    }
+  }
 </style>
